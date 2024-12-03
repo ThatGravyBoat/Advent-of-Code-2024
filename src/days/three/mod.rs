505fd9a0edba7b5faae0bc37895853bf071c22b2
Gravy Boat <gravy@thatgravyboat.tech>
2024-12-03 03:07:24 -0330

    Day 3

---
+++ src/days/three/mod.rs
@@ -0,0 +1,38 @@
+use std::fs;
+use regex::Regex;
+
+pub fn day_three_part_1() {
+    let contents = fs::read_to_string("./inputs/day_3.txt").unwrap();
+    let regex = Regex::new(r"mul\((\w{1,3}),(\d{1,3})\)").unwrap();
+
+    print!("{}", regex.captures_iter(contents.as_str())
+        .map(|captures| {
+            let (_, [first, second]) = captures.extract();
+            first.parse::<i32>().unwrap() * second.parse::<i32>().unwrap()
+        }).sum::<i32>()
+    );
+}
+
+pub fn day_three_part_2() {
+    let contents = fs::read_to_string("./inputs/day_3.txt").unwrap();
+    let regex = Regex::new(r"mul\(\w{1,3},\d{1,3}\)|don't\(\)|do\(\)").unwrap();
+
+    let mut active = true;
+
+    print!("{}", regex.find_iter(contents.as_str())
+        .map(|it| {
+            match it.as_str() {
+                "don't()" => active = false,
+                "do()" => active = true,
+                _ => {
+                    if active {
+                        let (first, second) = it.as_str().get(4..it.len() - 1).unwrap().split_once(",").unwrap();
+                        return first.parse::<i32>().unwrap() * second.parse::<i32>().unwrap();
+                    }
+                },
+            }
+            0
+        })
+        .reduce(|a, b| a + b)
+        .unwrap());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use crate::days::two::{day_two_part_1, day_two_part_2};
+use crate::days::three::{day_three_part_1, day_three_part_2};
 
 mod days;
 
 fn main() {
-    day_two_part_2();
+    day_three_part_2();
 }
